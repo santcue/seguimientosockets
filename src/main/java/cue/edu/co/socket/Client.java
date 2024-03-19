24be09e3dca044f8b929f9bfaff0c6c06960ae1a
santcue <sardila2225@cue.edu.co>
2024-03-19 09:27:47 -0500

    second commit

--- src/main/java/cue/edu/co/socket/Client.java
+++ src/main/java/cue/edu/co/socket/Client.java
@@ -7,103 +7,114 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class Client {
-    private Socket socket;
-    private DataInputStream bufferDeEntrada = null;
-    private DataOutputStream bufferDeSalida = null;
-    Scanner teclado = new Scanner(System.in);
-    final String COMANDO_TERMINACION = "salir()";
+    private Socket socket; // Socket para la conexión con el servidor
+    private DataInputStream bufferDeEntrada = null; // Flujo de entrada de datos desde el servidor
+    private DataOutputStream bufferDeSalida = null; // Flujo de salida de datos hacia el servidor
+    Scanner teclado = new Scanner(System.in); // Escáner para leer desde el teclado
+    final String COMANDO_TERMINACION = "salir()"; // Comando para terminar la conexión
 
+    // Método para establecer la conexión con el servidor
     public void levantarConexion(String ip, int puerto) {
         try {
-            socket = new Socket(ip, puerto);
-            mostrarTexto("Conectado a :" + socket.getInetAddress().getHostName());
+            socket = new Socket(ip, puerto); // Crea un nuevo socket con la dirección IP y el puerto especificados
+            mostrarTexto("Conectado a :" + socket.getInetAddress().getHostName()); // Muestra un mensaje de conexión exitosa
         } catch (Exception e) {
-            mostrarTexto("Excepción al levantar conexión: " + e.getMessage());
-            System.exit(0);
+            mostrarTexto("Excepción al levantar conexión: " + e.getMessage()); // Muestra un mensaje de excepción si no se puede conectar
+            System.exit(0); // Sale del programa
         }
     }
 
+    // Método auxiliar para imprimir texto en la consola
     public static void mostrarTexto(String s) {
         System.out.println(s);
     }
 
+    // Método para abrir los flujos de entrada y salida de datos
     public void abrirFlujos() {
         try {
-            bufferDeEntrada = new DataInputStream(socket.getInputStream());
-            bufferDeSalida = new DataOutputStream(socket.getOutputStream());
-            bufferDeSalida.flush();
+            bufferDeEntrada = new DataInputStream(socket.getInputStream()); // Crea un flujo de entrada de datos desde el servidor
+            bufferDeSalida = new DataOutputStream(socket.getOutputStream()); // Crea un flujo de salida de datos hacia el servidor
+            bufferDeSalida.flush(); // Limpia el flujo de salida
         } catch (IOException e) {
-            mostrarTexto("Error en la apertura de flujos");
+            mostrarTexto("Error en la apertura de flujos"); // Muestra un mensaje de error si no se pueden abrir los flujos
         }
     }
 
+    // Método para enviar un mensaje al servidor
     public void enviar(String s) {
         try {
-            bufferDeSalida.writeUTF(s);
-            bufferDeSalida.flush();
+            bufferDeSalida.writeUTF(s); // Escribe el mensaje en el flujo de salida
+            bufferDeSalida.flush(); // Limpia el flujo de salida
         } catch (IOException e) {
-            mostrarTexto("IOException on enviar");
+            mostrarTexto("IOException on enviar"); // Muestra un mensaje de error si ocurre una excepción de E/S
         }
     }
 
+    // Método para cerrar la conexión con el servidor
     public void cerrarConexion() {
         try {
-            bufferDeEntrada.close();
-            bufferDeSalida.close();
-            socket.close();
-            mostrarTexto("Conexión terminada");
+            bufferDeEntrada.close(); // Cierra el flujo de entrada
+            bufferDeSalida.close(); // Cierra el flujo de salida
+            socket.close(); // Cierra el socket
+            mostrarTexto("Conexión terminada"); // Muestra un mensaje de conexión terminada
         } catch (IOException e) {
-            mostrarTexto("IOException on cerrarConexion()");
-        }finally{
-            System.exit(0);
+            mostrarTexto("IOException on cerrarConexion()"); // Muestra un mensaje de error si ocurre una excepción de E/S
+        } finally {
+            System.exit(0); // Sale del programa
         }
     }
 
+    // Método para ejecutar la conexión con el servidor en un hilo separado
     public void ejecutarConexion(String ip, int puerto) {
         Thread hilo = new Thread(() -> {
             try {
-                levantarConexion(ip, puerto);
-                abrirFlujos();
-                recibirDatos();
+                levantarConexion(ip, puerto); // Establece la conexión con el servidor
+                abrirFlujos(); // Abre los flujos de entrada y salida de datos
+                recibirDatos(); // Recibe datos del servidor
             } finally {
-                cerrarConexion();
+                cerrarConexion(); // Cierra la conexión con el servidor
             }
         });
-        hilo.start();
+        hilo.start(); // Inicia el hilo
     }
 
+    // Método para recibir datos del servidor
     public void recibirDatos() {
-        String st = "";
+        String st = ""; // Variable para almacenar los datos recibidos
         try {
             do {
-                st = bufferDeEntrada.readUTF();
-                mostrarTexto("\n[Servidor] => " + st);
-                System.out.print("\n[Usted] => ");
-            } while (!st.equals(COMANDO_TERMINACION));
-        } catch (IOException e) {}
-    }
-
-    public void escribirDatos() {
-        String entrada = "";
-        while (true) {
-            System.out.print("[Usted] => ");
-            entrada = teclado.nextLine();
-            if(entrada.length() > 0)
-                enviar(entrada);
+                st = bufferDeEntrada.readUTF(); // Lee datos del flujo de entrada
+                mostrarTexto("\n[Servidor] => " + st); // Muestra los datos recibidos del servidor
+                System.out.print("\n[Usted] => "); // Muestra un indicador para que el usuario escriba
+            } while (!st.equals(COMANDO_TERMINACION)); // Continúa recibiendo datos hasta que se recibe el comando de terminación
+        } catch (IOException e) {
+            // Maneja cualquier excepción de E/S, pero no muestra ningún mensaje para evitar interrupciones continuas en la recepción de datos
         }
     }
 
-    public static void main(String[] argumentos) {
-        Client cliente = new Client();
-        Scanner escaner = new Scanner(System.in);
-        mostrarTexto("Ingresa la IP: [localhost por defecto] ");
-        String ip = escaner.nextLine();
-        if (ip.length() <= 0) ip = "localhost";
+    // Método para que el usuario escriba datos y los envíe al servidor
+    public void escribirDatos() {
+        String entrada = ""; // Variable para almacenar la entrada del usuario
+        while (true) {
+            System.out.print("[Usted] => "); // Muestra un indicador para que el usuario escriba
+            entrada = teclado.nextLine(); // Lee la entrada del usuario desde el teclado
+            if (entrada.length() > 0) // Verifica si se ha ingresado algún texto
+                enviar(entrada); // Envía la entrada del usuario al servidor
+        }
+    }
 
-        mostrarTexto("Puerto: [5050 por defecto] ");
-        String puerto = escaner.nextLine();
-        if (puerto.length() <= 0) puerto = "5050";
-        cliente.ejecutarConexion(ip, Integer.parseInt(puerto));
-        cliente.escribirDatos();
+    // Método principal del programa
+    public static void main(String[] argumentos) {
+        Client cliente = new Client(); // Crea una instancia del cliente
+        Scanner escaner = new Scanner(System.in); // Crea un escáner para leer desde el teclado
+        mostrarTexto("Ingresa la IP: [localhost por defecto] "); // Solicita al usuario que ingrese la dirección IP del servidor
+        String ip = escaner.nextLine(); // Lee la dirección IP ingresada por el usuario
+        if (ip.length() <= 0) ip = "localhost"; // Utiliza "localhost" como dirección IP predeterminada si no se proporciona ninguna
+
+        mostrarTexto("Puerto: [5050 por defecto] "); // Solicita al usuario que ingrese el puerto del servidor
+        String puerto = escaner.nextLine(); // Lee el puerto ingresado por el usuario
+        if (puerto.length() <= 0) puerto = "5050"; // Utiliza "5050" como puerto predeterminado si no se proporciona ninguno
+        cliente.ejecutarConexion(ip, Integer.parseInt(puerto)); // Ejecuta la conexión con el servidor
+        cliente.escribirDatos(); // Permite al usuario escribir y enviar datos al servidor
     }
 }
--- src/main/java/cue/edu/co/socket/Server.java
+++ src/main/java/cue/edu/co/socket/Server.java
@@ -8,104 +8,111 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class Server {
-    private Socket socket;
-    private DataInputStream bufferDeEntrada = null;
-    private DataOutputStream bufferDeSalida = null;
-    Scanner escaner = new Scanner(System.in);
-    final String COMANDO_TERMINACION = "salir()";
+    private Socket socket; // Socket para la conexión con el cliente
+    private DataInputStream bufferDeEntrada = null; // Flujo de entrada de datos desde el cliente
+    private DataOutputStream bufferDeSalida = null; // Flujo de salida de datos hacia el cliente
+    Scanner escaner = new Scanner(System.in); // Escáner para leer desde el teclado
+    final String COMANDO_TERMINACION = "salir()"; // Comando para terminar la conexión
 
+    // Método para levantar la conexión con el cliente en el puerto especificado
     public void levantarConexion(int puerto) {
         try {
-            ServerSocket serverSocket = new ServerSocket(puerto);
-            mostrarTexto("Esperando conexión entrante en el puerto " + String.valueOf(puerto) + "...");
-            socket = serverSocket.accept();
-            mostrarTexto("Conexión establecida con: " + socket.getInetAddress().getHostName() + "\n\n\n");
+            ServerSocket serverSocket = new ServerSocket(puerto); // Crea un ServerSocket en el puerto especificado
+            mostrarTexto("Esperando conexión entrante en el puerto " + String.valueOf(puerto) + "..."); // Muestra un mensaje de espera
+            socket = serverSocket.accept(); // Acepta la conexión entrante del cliente
+            mostrarTexto("Conexión establecida con: " + socket.getInetAddress().getHostName() + "\n\n\n"); // Muestra un mensaje de conexión establecida
         } catch (Exception e) {
-            mostrarTexto("Error en levantarConexion(): " + e.getMessage());
-            System.exit(0);
+            mostrarTexto("Error en levantarConexion(): " + e.getMessage()); // Muestra un mensaje de error si ocurre alguna excepción
+            System.exit(0); // Sale del programa
         }
     }
+
+    // Método para abrir los flujos de entrada y salida de datos
     public void flujos() {
         try {
-            bufferDeEntrada = new DataInputStream(socket.getInputStream());
-            bufferDeSalida = new DataOutputStream(socket.getOutputStream());
-            bufferDeSalida.flush();
+            bufferDeEntrada = new DataInputStream(socket.getInputStream()); // Crea un flujo de entrada de datos desde el cliente
+            bufferDeSalida = new DataOutputStream(socket.getOutputStream()); // Crea un flujo de salida de datos hacia el cliente
+            bufferDeSalida.flush(); // Limpia el flujo de salida
         } catch (IOException e) {
-            mostrarTexto("Error en la apertura de flujos");
+            mostrarTexto("Error en la apertura de flujos"); // Muestra un mensaje de error si ocurre una excepción de E/S
         }
     }
 
+    // Método para recibir datos del cliente
     public void recibirDatos() {
-        String st = "";
+        String st = ""; // Variable para almacenar los datos recibidos
         try {
             do {
-                st = bufferDeEntrada.readUTF();
-                mostrarTexto("\n[Cliente] => " + st);
-                System.out.print("\n[Usted] => ");
-            } while (!st.equals(COMANDO_TERMINACION));
+                st = bufferDeEntrada.readUTF(); // Lee datos del flujo de entrada
+                mostrarTexto("\n[Cliente] => " + st); // Muestra los datos recibidos del cliente
+                System.out.print("\n[Usted] => "); // Muestra un indicador para que el servidor escriba
+            } while (!st.equals(COMANDO_TERMINACION)); // Continúa recibiendo datos hasta que se recibe el comando de terminación
         } catch (IOException e) {
-            cerrarConexion();
+            cerrarConexion(); // Cierra la conexión con el cliente si hay un error de E/S
         }
     }
 
-
+    // Método para enviar un mensaje al cliente
     public void enviar(String s) {
         try {
-            bufferDeSalida.writeUTF(s);
-            bufferDeSalida.flush();
+            bufferDeSalida.writeUTF(s); // Escribe el mensaje en el flujo de salida
+            bufferDeSalida.flush(); // Limpia el flujo de salida
         } catch (IOException e) {
-            mostrarTexto("Error en enviar(): " + e.getMessage());
+            mostrarTexto("Error en enviar(): " + e.getMessage()); // Muestra un mensaje de error si ocurre una excepción de E/S
         }
     }
 
+    // Método auxiliar para imprimir texto en la consola
     public static void mostrarTexto(String s) {
         System.out.print(s);
     }
 
+    // Método para que el servidor escriba datos y los envíe al cliente
     public void escribirDatos() {
         while (true) {
-            System.out.print("[Usted] => ");
-            enviar(escaner.nextLine());
+            System.out.print("[Usted] => "); // Muestra un indicador para que el servidor escriba
+            enviar(escaner.nextLine()); // Lee la entrada del servidor desde el teclado y la envía al cliente
         }
     }
 
+    // Método para cerrar la conexión con el cliente
     public void cerrarConexion() {
         try {
-            bufferDeEntrada.close();
-            bufferDeSalida.close();
-            socket.close();
+            bufferDeEntrada.close(); // Cierra el flujo de entrada
+            bufferDeSalida.close(); // Cierra el flujo de salida
+            socket.close(); // Cierra el socket
         } catch (IOException e) {
-            mostrarTexto("Excepción en cerrarConexion(): " + e.getMessage());
+            mostrarTexto("Excepción en cerrarConexion(): " + e.getMessage()); // Muestra un mensaje de error si ocurre una excepción de E/S
         } finally {
-            mostrarTexto("Conversación finalizada....");
-            System.exit(0);
-
+            mostrarTexto("Conversación finalizada...."); // Muestra un mensaje de conversación finalizada
+            System.exit(0); // Sale del programa
         }
     }
 
+    // Método para ejecutar la conexión con el cliente en un hilo separado
     public void ejecutarConexion(int puerto) {
         Thread hilo = new Thread(() -> {
             while (true) {
                 try {
-                    levantarConexion(puerto);
-                    flujos();
-                    recibirDatos();
+                    levantarConexion(puerto); // Levanta la conexión con el cliente en el puerto especificado
+                    flujos(); // Abre los flujos de entrada y salida de datos
+                    recibirDatos(); // Recibe datos del cliente
                 } finally {
-                    cerrarConexion();
+                    cerrarConexion(); // Cierra la conexión con el cliente
                 }
             }
         });
-        hilo.start();
+        hilo.start(); // Inicia el hilo
     }
 
+    // Método principal del programa
     public static void main(String[] args) throws IOException {
-        Server s = new Server();
-        Scanner sc = new Scanner(System.in);
-
-        mostrarTexto("Ingresa el puerto [5050 por defecto]: ");
-        String puerto = sc.nextLine();
-        if (puerto.length() <= 0) puerto = "5050";
-        s.ejecutarConexion(Integer.parseInt(puerto));
-        s.escribirDatos();
+        Server s = new Server(); // Crea una instancia del servidor
+        Scanner sc = new Scanner(System.in); // Crea un escáner para leer desde el teclado
+        mostrarTexto("Ingresa el puerto [5050 por defecto]: "); // Solicita al usuario que ingrese el puerto del servidor
+        String puerto = sc.nextLine(); // Lee el puerto ingresado por el usuario
+        if (puerto.length() <= 0) puerto = "5050"; // Utiliza "5050" como puerto predeterminado si no se proporciona ninguno
+        s.ejecutarConexion(Integer.parseInt(puerto)); // Ejecuta la conexión con el cliente en el puerto especificado
+        s.escribirDatos(); // Permite al servidor escribir y enviar datos al cliente
     }
 }
